34af11fab32269e3081062e659222480800fafa4
Vr1sak <nebojsavm@gmail.com>
2025-07-03 16:14:50 +0200

    v2

--- src/app/employee-tabela/employee-tabela.ts
+++ src/app/employee-tabela/employee-tabela.ts
@@ -4,6 +4,9 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { ChartConfiguration, ChartOptions } from 'chart.js';
 import { NgChartsModule } from 'ng2-charts';
+import { ChartData, ChartType, Chart } from 'chart.js';
+import  ChartDataLabels  from 'chartjs-plugin-datalabels';
+Chart.register(ChartDataLabels);
 
 @Component({
   selector: 'app-employee-tabela',
@@ -20,6 +23,26 @@ export class EmployeeTabela {
   grupisani: { [name: string]: { EmployeeName: string; UkupnoSati: number } } = {};
   sortirani: { EmployeeName: string; UkupnoSati: number }[] = [];
 
+  public pieChartData: ChartData<'pie', number[], string> = {
+    labels: [],
+    datasets: [{ data: [] }]
+  };
+  pieChartType: 'pie' = 'pie';
+
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: true,
+    plugins: {
+      datalabels: {
+        color: '#000',
+        formatter: (value, ctx) => {
+          const data = ctx.chart.data.datasets[0].data as number[];
+          const sum = data.reduce((sum, v) => sum + (v as number), 0);
+          const procenat = (value as number) / sum * 100;
+          return `${procenat.toFixed(2)}%`;
+        }
+      }
+    }
+  };
 
   ngOnInit() {  
 
@@ -42,6 +65,10 @@ export class EmployeeTabela {
 
     this.sortirani = this.sortirani.sort((a, b) => b.UkupnoSati - a.UkupnoSati);
 
+    this.pieChartData = {
+          labels: this.sortirani.map(x => x.EmployeeName),
+          datasets: [{ data: this.sortirani.map(x => +x.UkupnoSati) }]
+        };
   });  
 
   }
